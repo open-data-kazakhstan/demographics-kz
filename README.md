b3f54c2212f84183b1e082297717aaff44a60bc0
anuveyatsu <anuar.ustayev@gmail.com>
2020-03-28 18:38:22 +0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,40 @@
+Population in Kazakhstan by region, age and sex.
+
+## Data
+
+Data is in CSV format and synced with upstream source monthly. It is sourced from https://data.egov.kz/datasets/view?index=kazakstan_respublikasy_halkyny1.
+
+We have processed the source data to make it normalized and derived from it several aggregated datasets:
+
+* `data/regions-age-sex.csv` - data by region, age and sex.
+* `data/regions-total.csv` - aggregated by region.
+* `data/regions-by-sex.csv` - aggregated by region and sex.
+
+We have also added some metadata such as column descriptions and [data packaged it][dp].
+
+[dp]: https://frictionlessdata.io/data-package/
+
+## Preparation
+
+[![Python 3.6](https://img.shields.io/badge/python-3.6-blue.svg)](https://www.python.org/downloads/release/python-360/)
+![.github/workflows/actions.yml](https://github.com/anuveyatsu/population-kz/workflows/.github/workflows/actions.yml/badge.svg?branch=master)
+
+This repository uses [dataflows](https://github.com/datahq/dataflows) to process and normalize the data.
+
+You first need to install the dependencies:
+
+```
+pip install -r scripts/requirements.txt
+```
+
+Then run the script
+
+```
+python scripts/process.py
+```
+
+## License
+
+This dataset is licensed under the Open Data Commons [Public Domain and Dedication License][pddl].
+
+[pddl]: https://www.opendatacommons.org/licenses/pddl/1-0/
---
+++ scripts/process.py
@@ -0,0 +1,74 @@
+from dataflows import Flow, load, dump_to_path, add_metadata, printer, update_resource, join_with_self
+from dataflows import find_replace, delete_fields, set_type, unpivot, update_schema, duplicate
+
+
+unpivoting_fields = [
+    { 'name': '(women|men)_(\d+(-|\+)\d*)', 'keys': {'sex': r'\1', 'age': r'\2'} }
+]
+extra_keys = [{'name': 'sex', 'type': 'string'}, {'name': 'age', 'type': 'string'}]
+extra_value = {'name': 'value', 'type': 'integer'}
+
+def remove_empty(rows):
+    for row in rows:
+        if row:
+            yield row
+
+def population():
+    flow = Flow(
+        # Load inputs
+        load('https://data.egov.kz/datasets/exportcsv?index=kazakstan_respublikasy_halkyny1&version=v3&from=1&count=100', format='csv', name='regions-age-sex'),
+        # Remove empty rows
+        remove_empty,
+        unpivot(unpivoting_fields, extra_keys, extra_value),
+        update_resource('regions-age-sex', path='data/regions-age-sex.csv'),
+        # Create total data
+        duplicate(
+            source='regions-age-sex',
+            target_name='regions-total',
+            target_path='data/regions-total.csv'
+        ),
+        join_with_self(
+            resource_name='regions-total',
+            join_key=['region'],
+            fields=dict(
+                region={
+                    'name': 'region'
+                },
+                value={
+                    'name': 'value',
+                    'aggregate': 'sum'
+                }
+            )
+        ),
+        # Create data by sex
+        duplicate(
+            source='regions-age-sex',
+            target_name='regions-by-sex',
+            target_path='data/regions-by-sex.csv'
+        ),
+        join_with_self(
+            resource_name='regions-by-sex',
+            join_key=['region', 'sex'],
+            fields=dict(
+                region={
+                    'name': 'region'
+                },
+                sex={
+                    'name': 'sex'
+                },
+                value={
+                    'name': 'value',
+                    'aggregate': 'sum'
+                }
+            )
+        ),
+        # Save the results
+        add_metadata(name='population-kz', title='''Population in Kazakhstan by region, age and sex.'''),
+        printer(),
+        dump_to_path(),
+    )
+    flow.process()
+
+
+if __name__ == '__main__':
+    population()
